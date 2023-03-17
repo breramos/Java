082e13b0af2cbb77058674428548609169795644
Brenda Ramos Bezerra <brendaramos.bezerra@gmail.com>
2023-03-16 22:08:37 -0300

    Updates das atividades da aula 2

--- aula_02/src/aula_02/Lista2Exercicio1.java
+++ aula_02/src/aula_02/Lista2Exercicio1.java
@@ -8,24 +8,48 @@ public class Lista2Exercicio1 {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		int item, quantidade;
+		float valorTotal;
+		int produto, quantidade;
 		
 		System.out.println("Lista de Produtos:");
-        System.out.println("1\tCachorro-Quente");
-        System.out.println("2\tX-Salada");
-        System.out.println("3\tX-Bacon");
-        System.out.println("4\tX-Bauru");
-        System.out.println("5\tRefrigerante");
-        System.out.println("6\tSuco de Laranja\n\n");
+        System.out.println("1\tCachorro-Quente.  R$ 10,00");
+        System.out.println("2\tX-Salada.  R$ 15,00");
+        System.out.println("3\tX-Bacon.  R$ 18,00");
+        System.out.println("4\tX-Bauru.  R$ 12,00");
+        System.out.println("5\tRefrigerante.  R$ 8,00");
+        System.out.println("6\tSuco de Laranja.  R$ 13,00\n\n");
         
         System.out.println("Digite o número correspondente ao seu pedido: ");
-		item = leia.nextInt();
+		produto = leia.nextInt();
 		
 		System.out.println("Digite a quantidade de produtos: ");
 		quantidade = leia.nextInt();
         
-        
+		
+		switch(produto) {
+			case 1: 
+				System.out.println("O valor total do produto é: " + (valorTotal = 10 * quantidade));
+			break;	
+			case 2:
+	            System.out.println("O valor do produto é: " + (valorTotal = 15 * quantidade));
+	            break;
+	        case 3:
+	            System.out.println("O valor do produto é: " + (valorTotal = 18 * quantidade));
+	            break;
+	        case 4:
+	            System.out.println("O valor do produto é: " + (valorTotal = 19 * quantidade));
+	            break;
+	        case 5:
+	            System.out.println("O valor do produto é: " + (valorTotal = 8 * quantidade));
+	            break;
+	        case 6:
+	            System.out.println("O valor do produto é: " + (valorTotal = 13 * quantidade));
+	            break;
+	        default:			   
+	                System.out.println("Produto Inválido");
+				
+				
+		}
 
 	}
-
 }
